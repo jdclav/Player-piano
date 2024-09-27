0070d6aee1d89a9abc117aae93ace253e03ce6a6
jdclav <jdclav62@gmail.com>
2024-09-27 00:30:23 -0400

    More work for the musicxml parser

--- converter_new.py
+++ converter_new.py
@@ -1,6 +1,19 @@
-for items in note_list:
+import os
+
+from musicxml import MusicXML
+
+# TODO Should be replaced by user input
+current_directory = os.path.dirname(os.path.realpath(__file__))
+file_name = "test.musicxml"
+
+xml_file = f"{current_directory}/{file_name}"
+
+music_xml = MusicXML(xml_file)
+
+
+"""for items in note_list:
     print(
         "=================================================================================================="
     )
     for item in items:
-        print(item)
+        print(item)"""
